430cf693ce9bf6b8180c17ff5cbf3961bcf381ff
Kay <khurrammohammed1990@gmail.com>
2019-10-15 01:07:19 +0100

    updating exercise component

--- exercise-tracker/src/components/create-exercise.component.js
+++ exercise-tracker/src/components/create-exercise.component.js
@@ -4,6 +4,12 @@ export default class CreateExercises extends Component {
     constructor(props) {
         super(props);
 
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeDuration = this.onChangeDuration.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        
         this.state = {
             username: '',
             description: '',
@@ -17,6 +23,38 @@ export default class CreateExercises extends Component {
         this.setState({
             username: e.target.value
         });
+    }
+
+    onChangeDescription(e) {
+        this.setState({
+            description: e.target.value
+        });
+    }
+
+    onChangeDuration(e) {
+        this.setState({
+            duration: e.target.value
+        });
+    }
+
+    onChangeDate(date) {
+        this.setState({
+            date: date
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const exercise = {
+            username = this.state.username,
+            description = this.state.description,
+            duration = this.state.duration,
+            date = this.state.date
+        }
+
+        console.log(exercise)
+
+        window.location = '/';
     }
 
     render() {
